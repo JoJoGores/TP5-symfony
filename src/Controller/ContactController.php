1d0ca0c4d1d5f57b51496a674138d4f60fd53c66
JoJoGores <jovangores@gmail.com>
2025-04-03 17:10:18 +0200

    seance du 03/04/2025

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,15 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\Repository\ContactRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contacts', name: 'app_contacts')]
-    public function listeContact()
+    #[Route('/contacts', name: 'app_contacts', methods: ['GET'])]
+    public function listeContact(ContactRepository $repo)
     {
-        return $this->render('contact/listeContacts.html.twig');
+        $Contacts = $repo->findAll();
+        return $this->render('contact/listeContacts.html.twig',[
+            'lesContacts' => $Contacts
+        ]);
+    }
+
+    #[Route('/contact/{id}', name: 'ficheContact', methods: ['GET'])]
+    public function ficheContact($id, ContactRepository $repo)
+    {
+        $Contact = $repo->find($id);
+        return $this->render('contact/ficheContact.html.twig',[
+            'leContact' => $Contact
+        ]);
     }
 }
+
+3min 40
